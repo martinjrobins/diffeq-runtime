d433a032a366b644aeea89eee2a1d44fccc0dda9
martinjrobins <martinjrobins@gmail.com>
2023-08-18 15:29:40 +0000

    compiling and running a sundials example

--- src/lib.h
+++ src/lib.h
@@ -71,3 +71,176 @@ static int check_retval(void *returnvalue, const char *funcname, int opt);
 /* Private function to check computed solution */
 
 static int check_ans(N_Vector y, realtype t, realtype rtol, N_Vector atol);
+
+/*
+ *-------------------------------
+ * Functions called by the solver
+ *-------------------------------
+ */
+
+/*
+ * f routine. Compute function f(t,y).
+ */
+
+static int f(realtype t, N_Vector y, N_Vector ydot, void *user_data)
+{
+  realtype y1, y2, y3, yd1, yd3;
+
+  y1 = Ith(y,1); y2 = Ith(y,2); y3 = Ith(y,3);
+
+  yd1 = Ith(ydot,1) = RCONST(-0.04)*y1 + RCONST(1.0e4)*y2*y3;
+  yd3 = Ith(ydot,3) = RCONST(3.0e7)*y2*y2;
+        Ith(ydot,2) = -yd1 - yd3;
+
+  return(0);
+}
+
+/*
+ * g routine. Compute functions g_i(t,y) for i = 0,1.
+ */
+
+static int g(realtype t, N_Vector y, realtype *gout, void *user_data)
+{
+  realtype y1, y3;
+
+  y1 = Ith(y,1); y3 = Ith(y,3);
+  gout[0] = y1 - RCONST(0.0001);
+  gout[1] = y3 - RCONST(0.01);
+
+  return(0);
+}
+
+/*
+ * Jacobian routine. Compute J(t,y) = df/dy. *
+ */
+
+static int Jac(realtype t, N_Vector y, N_Vector fy, SUNMatrix J,
+               void *user_data, N_Vector tmp1, N_Vector tmp2, N_Vector tmp3)
+{
+  realtype y2, y3;
+
+  y2 = Ith(y,2); y3 = Ith(y,3);
+
+  IJth(J,1,1) = RCONST(-0.04);
+  IJth(J,1,2) = RCONST(1.0e4)*y3;
+  IJth(J,1,3) = RCONST(1.0e4)*y2;
+
+  IJth(J,2,1) = RCONST(0.04);
+  IJth(J,2,2) = RCONST(-1.0e4)*y3-RCONST(6.0e7)*y2;
+  IJth(J,2,3) = RCONST(-1.0e4)*y2;
+
+  IJth(J,3,1) = ZERO;
+  IJth(J,3,2) = RCONST(6.0e7)*y2;
+  IJth(J,3,3) = ZERO;
+
+  return(0);
+}
+
+/*
+ *-------------------------------
+ * Private helper functions
+ *-------------------------------
+ */
+
+static void PrintOutput(realtype t, realtype y1, realtype y2, realtype y3)
+{
+#if defined(SUNDIALS_EXTENDED_PRECISION)
+  printf("At t = %0.4Le      y =%14.6Le  %14.6Le  %14.6Le\n", t, y1, y2, y3);
+#elif defined(SUNDIALS_DOUBLE_PRECISION)
+  printf("At t = %0.4e      y =%14.6e  %14.6e  %14.6e\n", t, y1, y2, y3);
+#else
+  printf("At t = %0.4e      y =%14.6e  %14.6e  %14.6e\n", t, y1, y2, y3);
+#endif
+
+  return;
+}
+
+static void PrintRootInfo(int root_f1, int root_f2)
+{
+  printf("    rootsfound[] = %3d %3d\n", root_f1, root_f2);
+
+  return;
+}
+
+/*
+ * Check function return value...
+ *   opt == 0 means SUNDIALS function allocates memory so check if
+ *            returned NULL pointer
+ *   opt == 1 means SUNDIALS function returns an integer value so check if
+ *            retval < 0
+ *   opt == 2 means function allocates memory so check if returned
+ *            NULL pointer
+ */
+
+static int check_retval(void *returnvalue, const char *funcname, int opt)
+{
+  int *retval;
+
+  /* Check if SUNDIALS function returned NULL pointer - no memory allocated */
+  if (opt == 0 && returnvalue == NULL) {
+    fprintf(stderr, "\nSUNDIALS_ERROR: %s() failed - returned NULL pointer\n\n",
+            funcname);
+    return(1); }
+
+  /* Check if retval < 0 */
+  else if (opt == 1) {
+    retval = (int *) returnvalue;
+    if (*retval < 0) {
+      fprintf(stderr, "\nSUNDIALS_ERROR: %s() failed with retval = %d\n\n",
+              funcname, *retval);
+      return(1); }}
+
+  /* Check if function returned NULL pointer - no memory allocated */
+  else if (opt == 2 && returnvalue == NULL) {
+    fprintf(stderr, "\nMEMORY_ERROR: %s() failed - returned NULL pointer\n\n",
+            funcname);
+    return(1); }
+
+  return(0);
+}
+
+/* compare the solution at the final time 4e10s to a reference solution computed
+   using a relative tolerance of 1e-8 and absoltue tolerance of 1e-14 */
+static int check_ans(N_Vector y, realtype t, realtype rtol, N_Vector atol)
+{
+  int      passfail=0;        /* answer pass (0) or fail (1) flag */
+  N_Vector ref;               /* reference solution vector        */
+  N_Vector ewt;               /* error weight vector              */
+  realtype err;               /* wrms error                       */
+  realtype ONE=RCONST(1.0);
+
+  /* create reference solution and error weight vectors */
+  ref = N_VClone(y);
+  ewt = N_VClone(y);
+
+  /* set the reference solution data */
+  NV_Ith_S(ref,0) = RCONST(5.2083495894337328e-08);
+  NV_Ith_S(ref,1) = RCONST(2.0833399429795671e-13);
+  NV_Ith_S(ref,2) = RCONST(9.9999994791629776e-01);
+
+  /* compute the error weight vector, loosen atol */
+  N_VAbs(ref, ewt);
+  N_VLinearSum(rtol, ewt, RCONST(10.0), atol, ewt);
+  if (N_VMin(ewt) <= ZERO) {
+    fprintf(stderr, "\nSUNDIALS_ERROR: check_ans failed - ewt <= 0\n\n");
+    return(-1);
+  }
+  N_VInv(ewt, ewt);
+
+  /* compute the solution error */
+  N_VLinearSum(ONE, y, -ONE, ref, ref);
+  err = N_VWrmsNorm(ref, ewt);
+
+  /* is the solution within the tolerances? */
+  passfail = (err < ONE) ? 0 : 1;
+
+  if (passfail) {
+    fprintf(stdout, "\nSUNDIALS_WARNING: check_ans error=%"GSYM"\n\n", err);
+  }
+
+  /* Free vectors */
+  N_VDestroy(ref);
+  N_VDestroy(ewt);
+
+  return(passfail);
+}
