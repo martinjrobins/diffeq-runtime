b4cd6bc9b3777fc42190be131466f9a5bc9f103b
martinjrobins <martinjrobins@gmail.com>
2023-11-12 15:24:22 +0000

    fix logistic growth test

--- tests/logistic_growth.c
+++ tests/logistic_growth.c
@@ -25,12 +25,12 @@ typedef double realtype;
 //}
 //
 // data = [r, k, F_0, F_1, G_0, G_1, out_0, out_1]
-#define R data[0]
-#define K data[1]
 #define Y u[0]
 #define Z u[1]
 #define DYDT up[0]
 #define DZDT up[1]
+#define R data[0]
+#define K data[1]
 #define F_0 data[2]
 #define F_1 data[3]
 #define G_0 data[4]
@@ -38,12 +38,12 @@ typedef double realtype;
 #define OUT_0 data[6]
 #define OUT_1 data[7]
 
-#define DR ddata[0]
-#define DK ddata[1]
 #define DY du[0]
 #define DZ du[1]
 #define DDYDT dup[0]
 #define DDZDT dup[1]
+#define DR ddata[0]
+#define DK ddata[1]
 #define DF_0 ddata[2]
 #define DF_1 ddata[3]
 #define DG_0 ddata[4]
@@ -54,7 +54,7 @@ typedef double realtype;
 
 void residual(const realtype t, const realtype* u, const realtype* up, realtype* data, const int* indices, realtype* rr) {
     F_0 = DYDT;
-    F_1 = DZDT;
+    F_1 = 0;
     G_0 = (R * Y) * (1 - (Y / K));
     G_1 = (2 * Y) - Z;
     rr[0] = F_0 - G_0;
@@ -63,8 +63,8 @@ void residual(const realtype t, const realtype* u, const realtype* up, realtype*
 
 void residual_grad(const realtype t, const realtype* u, const realtype* du, const realtype* up, const realtype* dup, realtype* data, realtype* ddata, const int* indices, realtype* rr, realtype* drr) {
     DF_0 = DDYDT;
-    DF_1 = DDZDT;
-    DG_0 = (DR * Y) * (1 - (Y / K)) + (R * DY) * (1 - (Y / K)) + (R * Y) * (0 - (DY / K));
+    DF_1 = 0;
+    DG_0 = (DR * Y) * (1 - (Y / K)) + (R * DY) * (1 - (Y / K)) + (R * Y) * (0 - (DY / K)) + (R * Y) * (0 + (Y * DK / (K * K)));
     DG_1 = (2 * DY) - DZ;
     drr[0] = DF_0 - DG_0;
     drr[1] = DF_1 - DG_1;
@@ -75,6 +75,8 @@ void set_u0(realtype* data, const int* indices, realtype* u, realtype* up) {
     Z = 0;
 }
 void set_u0_grad(realtype* data, realtype* ddata, const int* indices, realtype* u, realtype* du, realtype* up, realtype* dup) {
+    Y = 1;
+    Z = 0;
     DY = 0;
     DZ = 0;
 }
@@ -85,6 +87,8 @@ void calc_out(const realtype t, const realtype* u, const realtype* up, realtype*
 }
 
 void calc_out_grad(const realtype t, const realtype* u, const realtype* du, const realtype* up, const realtype* dup, realtype* data, realtype* ddata, const int* indices) {
+    OUT_0 = Y;
+    OUT_1 = Z;
     DOUT_0 = DY;
     DOUT_1 = DZ;
 }
@@ -94,6 +98,8 @@ void set_inputs(const realtype* inputs, realtype* data) {
     K = inputs[1];
 }
 void set_inputs_grad(const realtype* inputs, const realtype* dinputs, realtype* data, realtype* ddata) {
+    R = inputs[0];
+    K = inputs[1];
     DR = dinputs[0];
     DK = dinputs[1];
 }
@@ -105,8 +111,8 @@ void get_dims(int* states, int* inputs, int* outputs, int* data, const int* indi
     *data = 8;
 }
 void set_id(realtype* id) {
-    id[0] = 0;
-    id[1] = 1;
+    id[0] = 1;
+    id[1] = 0;
 }
 void get_out(const realtype* data, realtype** tensor_data, int* tensor_size) {
     *tensor_data = (realtype*) &OUT_0;
